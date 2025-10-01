8119859ddd105c8eac010a54b42cd223e7290588
t.k.langston <mail@designbytk.com>
2025-10-01 16:45:45 -0400

    Change database configuration from SQLite to PostgreSQL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,23 @@
 import "@/db/envConfig";
 import { defineConfig } from "drizzle-kit";
 
-// export default defineConfig({
-//   dialect: "postgresql",
-//   schema: "./db/schema.ts",
-//   out: "./db/migrations",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL!
-//   }
-// });
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
-  dialect: "sqlite", // Instead of "postgresql"
-  driver: "turso",   // Or "@libsql/client"
+  dialect: "postgresql",
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
   dbCredentials: {
-    url: process.env.SQLITE_URL!,
-    authToken: process.env.SQLITE_AUTH_TOKEN, // if required
-  },
-  verbose: true,
-  strict: true,
+    url: process.env.DATABASE_URL!
+  }
 });
+// export default defineConfig({
+//   schema: "./src/db/schema.ts",
+//   out: "./src/db/migrations",
+//   dialect: "sqlite", // Instead of "postgresql"
+//   driver: "turso",   // Or "@libsql/client"
+//   dbCredentials: {
+//     url: process.env.SQLITE_URL!,
+//     authToken: process.env.SQLITE_AUTH_TOKEN, // if required
+//   },
+//   verbose: true,
+//   strict: true,
+// });
